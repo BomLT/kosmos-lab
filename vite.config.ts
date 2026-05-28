3bf8768aeb8ec212bbad3f516fb8bc7ac2a25259
BomLT <ltran@lyell.com>
2026-05-27 20:01:45 -0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/kosmos-lab/',
   build: {
     outDir: 'dist',
     chunkSizeWarningLimit: 3000,
